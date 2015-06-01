b014d0e414f434cd0a61c49580adc86b416b73e3
dompisko <dominika.piskosz@vp.pl>
2015-06-01 17:53:09 +0200

    Zmiany
    Zmieniłam tak, że zamiast kółek mogą być wrzucone zdjęcia, ale na razie
    jest to zakomentowane, bo próbowałam też ogarnąć menu, przyciski
    działają, ale nie wiem jak zmieniać tryb i to co jest zakomentowane
    powinno być tryb = 0

--- src/etishooter/ETIShooter.java
+++ src/etishooter/ETIShooter.java
@@ -2,6 +2,8 @@ package etishooter;
 
 import com.sun.java.accessibility.util.AWTEventMonitor;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.geom.Ellipse2D;
@@ -46,15 +48,42 @@ class Cel
 }
 
 
-public class ETIShooter extends JFrame {
-    
+public class ETIShooter extends JFrame 
+{
+    private JButton przyciski[];
+    private JTextArea oknoTekstowe;
     private int punktacja = 0;
     private int tryb = 0;
     private Cel cele[];
-    private Image tlo;
+    private Image tlo1;
+    private Image celDobry;
+    private Image celZly;
     private Timer zegar;
     private Rectangle granice = new Rectangle(50,50,700,500);
     private boolean CzyGenerowac = true;
+    
+    private class ObslugaPrzycisku implements ActionListener
+    {
+        private JFrame ref_okno;
+
+        ObslugaPrzycisku(JFrame okno)
+        {
+         ref_okno = okno;
+        }
+
+        public void actionPerformed(ActionEvent e) 
+        {
+            JButton bt = (JButton)e.getSource();
+            if(bt==przyciski[0])
+            {
+                oknoTekstowe.append("Najlepsze wyniki:\n");
+            }                
+            else if(bt==przyciski[1])
+            {
+                tryb = 1; 
+            }         
+        }
+    }
     
     class Zadanie extends TimerTask
     {
@@ -78,7 +107,8 @@ public class ETIShooter extends JFrame {
         }
     }
     
-    ETIShooter(){
+    ETIShooter()
+    {
         super("ETI Shooter");
         cele = new Cel[10];
         setBounds(50,50,800,600);
@@ -86,19 +116,44 @@ public class ETIShooter extends JFrame {
         setResizable(false);
         setVisible(true);
         createBufferStrategy(2);
+       
+        przyciski = new JButton[2];
+        
+        przyciski[0] = new JButton("NAJLEPSZE WYNIKI");
+        przyciski[0].addActionListener(new ObslugaPrzycisku(this));
 
+        przyciski[1] = new JButton("NOWA GRA");
+        przyciski[1].addActionListener(new ObslugaPrzycisku(this));
+
+        JPanel panelPrzyciski   = new JPanel(new FlowLayout());
+        panelPrzyciski.add(przyciski[0]);
+        panelPrzyciski.add(przyciski[1]);
+
+        oknoTekstowe = new JTextArea("ETIShooter\n");
+
+        Container content = getContentPane();
+        content.setLayout(new BorderLayout());
+
+        content.add(panelPrzyciski,BorderLayout.NORTH);
+        content.add(new JScrollPane(oknoTekstowe),BorderLayout.CENTER);
+        setVisible(true);
+        
+        
         zegar = new java.util.Timer();
         zegar.scheduleAtFixedRate(new Zadanie(),0,2000);
         
-        this.addMouseListener(new MouseListener(){
+        this.addMouseListener(new MouseListener()
+        {
 
             @Override
-            public void mouseClicked(MouseEvent e) {
+            public void mouseClicked(MouseEvent e) 
+            {
                 
             }
-
+            
             @Override
-            public void mousePressed(MouseEvent e) {
+            public void mousePressed(MouseEvent e) 
+            {
                 Point trafione = e.getPoint();
                 for (int i = 0; i < 10; i++)
                 {
@@ -116,22 +171,27 @@ public class ETIShooter extends JFrame {
             }
 
             @Override
-            public void mouseReleased(MouseEvent e) {
+            public void mouseReleased(MouseEvent e) 
+            {
                 
             }
 
             @Override
-            public void mouseEntered(MouseEvent e) {
+            public void mouseEntered(MouseEvent e) 
+            {
                 
             }
 
             @Override
-            public void mouseExited(MouseEvent e) {
+            public void mouseExited(MouseEvent e) 
+            {
+                
             }
         });
         
-        tlo = new ImageIcon("res/tlo.jpg").getImage();
-              
+        tlo1 = new ImageIcon("res/tlo1.jpeg").getImage();
+        celDobry = new ImageIcon("res/celDobry.jpg").getImage();
+        celZly = new ImageIcon("res/celZly.jpg").getImage();
     }    
     
     public static void main(String[] args) 
@@ -140,7 +200,7 @@ public class ETIShooter extends JFrame {
         okno.repaint();
     }
     
-    public void paint(Graphics g)
+ /*   public void paint(Graphics g)
     {
         try 
         {
@@ -155,7 +215,7 @@ public class ETIShooter extends JFrame {
         Graphics2D g2d = (Graphics2D)bstrategy.getDrawGraphics();
         
         
-        g2d.drawImage(tlo, 0, 0, 800 , 600, null);
+        g2d.drawImage(tlo1, 0, 0, 800 , 600, null);
         g2d.setColor(Color.RED);
         g2d.setFont(new Font("Arial",Font.BOLD,20));
         g2d.drawString("PUNKTACJA: " + punktacja, 5, 55);
@@ -163,31 +223,31 @@ public class ETIShooter extends JFrame {
         for (int i=0; i < 10; i++)
         {
             if (!cele[i].GetTrafiony())
-            {
+           {
                 try
                 {
                     if (cele[i].GetDobry())
-                        g2d.setColor(Color.GREEN);
+                        g2d.drawImage(celDobry, 
+                                      cele[i].GetKwadratura().x,
+                                      cele[i].GetKwadratura().y,
+                                      cele[i].GetKwadratura().width,
+                                      cele[i].GetKwadratura().height,
+                                      null);
                     else
-                        g2d.setColor(Color.RED);
+                       g2d.drawImage(celZly, 
+                                     cele[i].GetKwadratura().x,
+                                     cele[i].GetKwadratura().y,
+                                     cele[i].GetKwadratura().width,
+                                     cele[i].GetKwadratura().height,
+                                     null);
                 }
                 catch (NullPointerException e)
                 {
                     continue;
                 }
-                Ellipse2D Elipsa = new Ellipse2D.Float( cele[i].GetKwadratura().x,
-                                                        cele[i].GetKwadratura().y,
-                                                        cele[i].GetKwadratura().width,
-                                                        cele[i].GetKwadratura().height);
-                g2d.draw(Elipsa);
-                g2d.fill(Elipsa);
-            }
-        }
-        
+           }
+        }        
         g2d.dispose();
-        
-        bstrategy.show();
-        
-    }
-    
+        bstrategy.show();        
+    }   */ 
 }
